06c1b56b6527194f8a9d52337933504d9526ab16
navigator515 <gksrlf515@hanmail.net>
2020-08-11 11:47:05 +0900

    src 파일 추가, 143p까지 구현

---
+++ build/resources/main/static/js/app/index.js
@@ -0,0 +1,31 @@
+var main={
+    init : function(){
+        var _this=this;
+        $('#btn-save').init('click',function(){
+            _this.save();
+        });
+    },
+    save:function(){
+        var data={
+            title:$('#title').val(),
+            author:$('#author').val(),
+            content:$('#content').val()
+
+        };
+
+        $.ajax({
+            type: 'POST',
+            url:'/api/v1/posts',
+            dataType:'json',
+            contentType:'application/json; charset=utf-8',
+            date: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 등록되었습니다.');
+            window.location.href='/';
+
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    }
+};
+index.init();
