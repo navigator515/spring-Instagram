5c8429a2dde8922fa641f30c31b1bd2614fce15f
navigator515 <gksrlf515@hanmail.net>
2020-08-13 16:29:29 +0900

    ./gradlew test 오류 수정3

--- src/main/java/com/hangil/book/springboot/domain/user/User.java
+++ src/main/java/com/hangil/book/springboot/domain/user/User.java
@@ -4,14 +4,7 @@ import com.hangil.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
