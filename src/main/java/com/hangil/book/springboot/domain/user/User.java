9ed5e6f516a27434d24cde6017c705258bdca98f
navigator515 <gksrlf515@hanmail.net>
2020-08-13 15:53:58 +0900

    ./gradlew test 오류 수정2

--- src/main/java/com/hangil/book/springboot/domain/user/User.java
+++ src/main/java/com/hangil/book/springboot/domain/user/User.java
@@ -5,42 +5,52 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class User extends BaseTimeEntity {
-@Id
-@GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
-@Column(nullable=false)
-    private String name;
-@Column
-    private String email;
-@Column
-    private String picture;
-@Enumerated(EnumType.STRING)
 
-@Column(nullable=false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
-@Builder
-    public User(String name, String email, String picture,Role role){
-    this.name=name;
-    this.email=email;
-    this.picture=picture;
-    this.role=role;
-}
-public User update(String name, String picture)
-{
-    this.name=name;
-    this.picture=picture;
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
 
-    return this;
-}
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
 
-public String getRoleKey(){
-    return this.role.getKey();
-}
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
